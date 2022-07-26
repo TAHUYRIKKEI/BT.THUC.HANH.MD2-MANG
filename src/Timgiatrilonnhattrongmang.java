686d800a985403c56557413c71ffe464c78a72d9
unknown <tavanhuyvp8590@gmail.com>
2022-07-27 00:08:35 +0700

    BAI TAP THUC HANH MANG

---
+++ src/Timgiatrilonnhattrongmang.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Timgiatrilonnhattrongmang {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size");
+            size = scanner.nextInt();
+            if (size > 20) {
+                System.out.println("size should not exceed 20");
+            }while (size > 20) ;
+            array = new int[size];
+            int i = 0;
+            while (i < array.length) {
+                System.out.print("Enter element" + (i + 1) + " : ");
+                i++;
+            }
+            System.out.print("property list:");
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[j] + "\t");
+
+            }
+            int max = array[0];
+            int index = 1;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] > max) ;
+                max = array[j];
+                index = j + 1;
+
+            }
+        }
+         System.out.println("the largest property value in the list is " + max +",at position"+ index);
+    }
+}
+
